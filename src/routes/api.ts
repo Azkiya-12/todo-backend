e65934095863ed894c653e33cf1d065dfa5d361b
Azkiya-12 <azkiyafazaputripratama@gmail.com>
2026-09-07 22:13:33 +0700

    first commit

---
+++ src/routes/api.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { register, login } from '../controllers/authController';
+import { getTodos, createTodo } from '../controllers/todoController';
+import {validateRegister, validateLogin, validateTodo} from '../middlewares/validator';
+import { verifyToken } from '../middlewares/authMiddleware';
+
+const router = Router();
+
+// AUTTHENTICATION ROUTES
+router.post('/auth/register', validateRegister, register);
+router.post('/auth/login', validateLogin, login);
+
+// TODO ROUTES (protected)
+router.get('/todos', verifyToken, getTodos);
+router.post('/todos', verifyToken, validateTodo, createTodo);
+
+export default router;
